727fee1955d9875d4dbb2a42c25d43c1bbc0caf3
Kalitg28 <157677832+Kalitg28@users.noreply.github.com>
2025-05-02 12:49:52 +0530

    Update info.py

--- info.py
+++ info.py
@@ -113,15 +113,15 @@ USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', True))
 
 # Token Verification Info :
 VERIFY = bool(environ.get('VERIFY', True))
-VERIFY_SHORTLINK_URL = environ.get('VERIFY_SHORTLINK_URL', 'shrinkearn.com')
-VERIFY_SHORTLINK_API = environ.get('VERIFY_SHORTLINK_API', 'f838cfea4afe762d0ff9d28d41f9202ea6fe414d')
+VERIFY_SHORTLINK_URL = environ.get('VERIFY_SHORTLINK_URL', 'OmegaLinks.in')
+VERIFY_SHORTLINK_API = environ.get('VERIFY_SHORTLINK_API', 'b10837a4b38b14476d8fb64e1a02d1610160c9ce')
 VERIFY_TUTORIAL = environ.get('VERIFY_TUTORIAL', 'https://t.me/How_To_Download_MVS/5')
 
 # If You Fill Second Shortner Then Bot Attach Both First And Second Shortner And Use It For Verify.
 VERIFY_SECOND_SHORTNER = bool(environ.get('VERIFY_SECOND_SHORTNER', True))
 # if verify second shortner is True then fill below url and api
-VERIFY_SND_SHORTLINK_URL = environ.get('VERIFY_SND_SHORTLINK_URL', 'shrinkearn.com')
-VERIFY_SND_SHORTLINK_API = environ.get('VERIFY_SND_SHORTLINK_API', 'f838cfea4afe762d0ff9d28d41f9202ea6fe414d')
+VERIFY_SND_SHORTLINK_URL = environ.get('VERIFY_SND_SHORTLINK_URL', 'OmegaLinks.in')
+VERIFY_SND_SHORTLINK_API = environ.get('VERIFY_SND_SHORTLINK_API', 'b10837a4b38b14476d8fb64e1a02d1610160c9ce')
 
 
 # Shortlink Info
